61f1225412aa7a74ee0fd6e774a35f81f8dd4697
Surendra <surendra.mundvadiya@softsenaor.ai>
2022-11-27 15:53:51 +0530

    fix

--- src/components/ComentsPage.js
+++ src/components/ComentsPage.js
@@ -1,23 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import Coments from "../api/Coments";
 import Environment from "../Environment";
 import { FixedSizeList as List } from "react-window";
 import AutoSixer from "react-virtualized-auto-sizer";
-
-const random_sprit = [
-    // "male",
-    "female",
-    // "human",
-    // "identicon",
-    // "initials",
-    // "bottts",
-    // "avataaars",
-    // "jdenticon",
-    // "gridy",
-    // "micah",
-];
-
-const get_sprit = () => random_sprit[Math.floor(Math.random() * random_sprit.length)];
 
 const ComentsPage = () => {
     const [coments, setComents] = useState([]);
@@ -25,7 +10,6 @@ const ComentsPage = () => {
         Coments.getComments()
             .then((res) => {
                 if (res.status === 200) {
-                    console.log(res);
                     setComents(res.data);
                 } else {
                     setComents([]);
@@ -45,7 +29,6 @@ const ComentsPage = () => {
                     <img
                         alt="ecommerce"
                         className="object-cover object-center w-20 h-20 block border-2 rounded-full"
-                        // src={Environment.IMAGE_URL + "/" + get_sprit() + "/" + el.name + ".svg"}
                         src={
                             Environment.IMAGE_URL +
                             "/" +
@@ -69,29 +52,6 @@ const ComentsPage = () => {
     };
     return (
         <div className="h-5/6 w-3/6 overflow-y-auto  border-2 rounded p-2">
-            {/* {coments.map((el) => {
-                return (
-                    <>
-                        <div className="container mx-auto flex items-center p-2 w-full  my-4 ">
-                            <img
-                                alt="ecommerce"
-                                className="object-cover object-center w-20 h-20 block border-2 rounded-full"
-                                src={Environment.IMAGE_URL + "/" + el.name + ".svg"}
-                            />
-                            <div className="ml-4">
-                                <h2 className="text-gray-900 title-font text-lg font-medium">
-                                    {el.name}
-                                </h2>
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mt-1">
-                                    {el.email}
-                                </h3>
-                                <p className="mt-1 w-5/6 ">{el.body}</p>
-                            </div>
-                        </div>
-                        <div className="h-px bg-gray-300"></div>
-                    </>
-                );
-            })} */}
             <AutoSixer>
                 {({ height, width }) => {
                     return (
@@ -110,4 +70,4 @@ const ComentsPage = () => {
     );
 };
 
-export default ComentsPage;
+export default memo(ComentsPage);
